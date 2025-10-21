f01d330f2b1a496ee8b7a41e049e3d91af0c4343
Leandro Alves <leandro.silva.alves86@gmail.com>
2025-10-21 18:54:06 -0300

    Transform worker in continous processing

--- src/worker_app/program.py
+++ src/worker_app/program.py
@@ -2,6 +2,7 @@ from config.log_config import logger
 from database import db_tables
 from database.db import connect, close as db_close
 import worker
+from time import sleep
 
 logger.info("Starting the application...")
 
@@ -9,5 +10,8 @@ db = connect()
 db_tables.setup_database(db)
 db_close()
 
-worker.process()
-logger.info("Application finished.")
+while True:
+    logger.info("Starting process.")
+    worker.process()
+    logger.info("Process finished.")
+    sleep(5)
